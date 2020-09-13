1654f5e4326d845d576685577ef2c15b65670550
jricebae19 <68259136+jricebae19@users.noreply.github.com>
2020-09-13 15:00:58 -0700

    completed 9.5.4

--- app.py
+++ app.py
@@ -52,3 +52,10 @@ def precipitation():
         filter(Measurement.date >= prev_year).all()
     precip = {date: prcp for date, prcp in precipitation}
     return jsonify(precip)
+
+#Add stations route
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
